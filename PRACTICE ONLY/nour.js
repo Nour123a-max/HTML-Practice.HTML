cc2a498030b9f256ff43033b789b3b05457ba8b7
Nour123a-max <Abdelatif.nour@gmail.com>
2025-07-24 17:27:03 -0400

    Delete PRACTICE ONLY directory

--- PRACTICE ONLY/nour.js
+++
@@ -1,7 +0,0 @@
-// Create an axios instance with a base URL
-const api = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com/' // Change this to your desired base URL
-});
-
-// Example usage:
-// api.get('users').then(response => console.log(response.data));
